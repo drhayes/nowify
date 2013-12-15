836fbe96c4fc8dda0524c9a95fe8917798fd7036
David Hayes <drhayes@gmail.com>
2013-12-14 20:07:49 -0600

    Duh. Don't return a function, just return now.

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 
 'use strict';
 
+// Start date for faking performance.now because IE sucks and doesn't have
+// Date.now.
+var start = +new Date();
+
 module.exports = function() {
   if (global.performance && global.performance.now) {
-    return global.performance.now.bind(global.performance);
+    return global.performance.now();
   }
 
-  // Fake it. Use +new Date() because IE sucks and doesn't have .now().
-  var start = +new Date();
-
-  return function() {
-    return (+new Date() - start);
-  };
+  // Fake it. Use +new Date because IE is stupid.
+  return (+new Date() - start);
 };
--- test/indexTest.js
+++ test/indexTest.js
@@ -3,7 +3,7 @@
 
 'use strict';
 
-var perfnow = require('../index');
+var now = require('../index');
 var assert = require('assert');
 var sinon = require('sinon');
 
@@ -12,14 +12,12 @@ describe('performance.now polyfill', function() {
     global.performance = {
       now: sinon.spy()
     };
-    var now = perfnow();
     now();
     assert(global.performance.now.called);
     delete global.performance;
   });
 
   it('uses Date if it cannot find performance.now', function(done) {
-    var now = perfnow();
     var start = now();
     setTimeout(function() {
       var end = now();
@@ -30,13 +28,13 @@ describe('performance.now polyfill', function() {
   });
 
   it('is still monotonic without real performance.now', function(done) {
-    var now = perfnow();
     var one = now();
     setTimeout(function() {
       var two = now();
       setTimeout(function() {
         var three = now();
-        assert(three > two > one);
+        assert(three > two);
+        assert(two > one);
         done();
       }, 10);
     }, 10);
